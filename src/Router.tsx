e321c6e87833bf58f4bca032f20d22dd24abbb42
cathyshx <cacadasilva29@gmail.com>
2023-02-22 12:23:25 -0300

    Layout e rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,12 +3,15 @@ import { Routes, Route } from 'react-router-dom'
 import { History } from './pages/History'
 
 import { Home } from './pages/Home'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/History" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/History" element={<History />} />
+      </Route>
     </Routes>
   )
 }
