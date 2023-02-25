decd212560764f8c3edd4a7d1dadb61d920525fe
cathyshx <cacadasilva29@gmail.com>
2023-02-25 20:22:13 -0300

    Página: History

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import { History } from './pages/History'
 
-import { Home } from './pages/home'
+import { Home } from './pages/Home'
 import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
